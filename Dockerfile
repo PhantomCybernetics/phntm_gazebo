4641d98560fd9c82b161664f10981c0ef64a3bc8
Mirek Burkon <mirek@phntm.io>
2025-07-05 05:30:21 +0000

    gazebo from source w forked gz-sensors

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,9 @@ RUN apt-get update -y --fix-missing
 # dev conveniences
 RUN apt-get install -y vim mc \
                        iputils-ping net-tools iproute2 \
-                       curl
+                       curl \
+                       gdb \
+                       clangd
 
 RUN apt-get install -y lsb-release gnupg
 
@@ -16,25 +18,46 @@ RUN apt-get install -y lsb-release gnupg
 RUN curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
 RUN echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
 RUN apt-get update
-RUN apt-get install -y gz-harmonic
+#RUN apt-get install -y gz-harmonic
 
 RUN apt-get install -y ros-$ROS_DISTRO-ros-gz \
     ros-$ROS_DISTRO-gz-ros2-control \
     ros-$ROS_DISTRO-ros2-control \
     ros-$ROS_DISTRO-ros2-controllers \
-    ros-$ROS_DISTRO-navigation2 \
     ros-$ROS_DISTRO-controller-manager
+    #ros-$ROS_DISTRO-navigation2 \
     # ros-$ROS_DISTRO-nav2-bringup \
     # ros-$ROS_DISTRO-slam-toolbox
+
+# init workspace
+ENV ROS_WS=/ros2_ws
+RUN mkdir -p $ROS_WS/src
+WORKDIR $ROS_WS
+COPY ./ $ROS_WS/src/simbot_gz
+
+# Gazebo Harmonic from source with custom gz-sim
+RUN apt install -y python3-pip python3-venv
+RUN python3 -m venv /root/vcs_colcon_installation
+RUN . /root/vcs_colcon_installation/bin/activate && \
+    pip install vcstool colcon-common-extensions jinja2 typeguard
+
+ENV GZ_WS=/gz_ws
+RUN mkdir -p $GZ_WS/src
+WORKDIR $GZ_WS/src
+# custom collection-harmonic without gz-gui and using custom gz-sensors
+RUN ln -s $ROS_WS/src/simbot_gz/collection-harmonic-custom.yaml $GZ_WS/src/collection-harmonic-custom.yaml
+RUN vcs import < collection-harmonic-custom.yaml
+RUN apt -y install $(sort -u $(find . -iname 'packages-'`lsb_release -cs`'.apt' -o -iname 'packages.apt' | grep -v '/\.git/') | sed '/gz\|sdf/d' | tr '\n' ' ')
+# TODO this also installs x-server (??)
+WORKDIR $GZ_WS
+RUN colcon build --cmake-args ' -DBUILD_TESTING=OFF' ' -DCMAKE_BUILD_TYPE=Release' ' -DSKIP_optix=true' --symlink-install
+
 
 # RUN apt-get install -y \
 #     libgz-common6-dev \
 #     libgz-plugin3-dev \
 #     libgz-rendering9-dev
 
-# init workspace
-ENV ROS_WS=/ros2_ws
-RUN mkdir -p $ROS_WS/src
 WORKDIR $ROS_WS
 
 # clone and install Phntm Interfaces
@@ -55,12 +78,21 @@ RUN . /opt/ros/$ROS_DISTRO/setup.sh && \
 # RUN apt install -y cuda-drivers
 # WORKDIR $ROS_WS
 
+# RUN apt remove apt remove ros-$ROS_DISTRO-gz-sensors-vendor
+# RUN apt remove libgz-sensors8
+
 # h264 video output
 RUN apt install -y ffmpeg
 RUN apt install -y libnvidia-encode-535
 
+# gz-sensors fork
+# RUN git clone https://github.com/gazebosim/gz-sensors /root/gz-sensors
+# WORKDIR /root/gz-sensors
+# RUN git checkout gz-sensors8
+# RUN mkdir build; cd build; cmake ..; make
+
 # build the sim
-COPY ./ $ROS_WS/src/simbot_gz
+WORKDIR $ROS_WS
 RUN . /opt/ros/$ROS_DISTRO/setup.sh && \
     . /ros2_ws/install/setup.sh && \
     rosdep install -i --from-path src/simbot_gz --rosdistro $ROS_DISTRO -y && \
@@ -73,6 +105,7 @@ set -e \n \
 # setup ros environment \n \
 source "/opt/ros/'$ROS_DISTRO'/setup.bash" \n \
 test -f "/ros2_ws/install/setup.bash" && source "/ros2_ws/install/setup.bash" \n \
+test -f "/gz_ws/install/setup.bash" && source "/gz_ws/install/setup.bash" \n \
 \n \
 exec "$@"' > /ros_entrypoint.sh
 RUN chmod a+x /ros_entrypoint.sh
@@ -80,6 +113,7 @@ RUN chmod a+x /ros_entrypoint.sh
 # source underlay on every login
 RUN echo 'source /opt/ros/'$ROS_DISTRO'/setup.bash' >> /root/.bashrc
 RUN echo 'test -f "/ros2_ws/install/setup.bash" && source "/ros2_ws/install/setup.bash"' >> /root/.bashrc
+RUN echo 'test -f "/gz_ws/install/setup.bash" && source "/gz_ws/install/setup.bash"' >> /root/.bashrc
 
 # set startup path
 WORKDIR $ROS_WS
--- launch/simbot_gz.launch.py
+++ launch/simbot_gz.launch.py
@@ -1,6 +1,7 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import OpaqueFunction
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable, EmitEvent, AppendEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
@@ -12,7 +13,7 @@ from pathlib import Path
 PACKAGE_NAME = "simbot_gz"
 
 ARGUMENTS = [
-    DeclareLaunchArgument('world', default_value='maze_2_brick.sdf', description='Gazebo World file (with extension)'),
+    DeclareLaunchArgument('world', default_value='demo_world.sdf', description='Gazebo World file (with extension)'),
     DeclareLaunchArgument(
             'use_sim_time',
             default_value='true',
@@ -28,7 +29,7 @@ robot_model_list = [
     'simbot_mecanum_waffle'
 ]
 
-def generate_launch_description():
+def launch_setup(context, *args, **kwargs):
     robot_model = os.environ.get("ROBOT_MODEL", "simbot_mecanum_waffle")
 
     if robot_model not in robot_model_list:
@@ -46,32 +47,38 @@ def generate_launch_description():
     print("Robot model: ", robot_model)
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
 
+    # ld = LaunchDescription(ARGUMENTS)
+    actions = []
+    
     # Source Environment (Need it to be able find mesh files)
     pkg_path = get_package_share_directory(PACKAGE_NAME)
-    ign_resource_path_worlds = AppendEnvironmentVariable(
+    actions.append(AppendEnvironmentVariable(
         name='GZ_SIM_RESOURCE_PATH',
         value=PathJoinSubstitution([pkg_path, 'worlds'])
-    )
-    ign_resource_path_meshes = AppendEnvironmentVariable(
+    ))
+    
+    actions.append(AppendEnvironmentVariable(
         name='GZ_SIM_RESOURCE_PATH',
         value=PathJoinSubstitution([pkg_path, 'meshes'])
-    )
+    ))
     
     # Add custom model path
-    ign_resource_path_models = AppendEnvironmentVariable(
+    actions.append(AppendEnvironmentVariable(
         'GZ_SIM_RESOURCE_PATH',
         PathJoinSubstitution([pkg_path, 'models'])
-    )
+    ))
     
     append_ign_path = AppendEnvironmentVariable(
         name='IGN_GAZEBO_RESOURCE_PATH',
         value='/ros2_ws/src/'
     )
+    actions.append(append_ign_path)    
     
     append_gz_path = AppendEnvironmentVariable(
         name='GZ_SIM_RESOURCE_PATH',
         value='/ros2_ws/src/'
     )
+    actions.append(append_gz_path)
 
     # Create a robot_state_publisher node
     pkg_path = get_package_share_directory(PACKAGE_NAME)
@@ -87,46 +94,54 @@ def generate_launch_description():
     print(f"Creating state publisher...")
 
     params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time, 'publish_frequency': 30.0}
-    node_robot_state_publisher = Node(
+    actions.append(Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
         parameters=[params]
-    )
+    ))
 
-    world_path = PathJoinSubstitution([pkg_path, 'worlds', LaunchConfiguration('world')])
+    world_sdf_path = PathJoinSubstitution([pkg_path, 'worlds', LaunchConfiguration('world')])
 
     # launch gazebo
-    print(f"Launching Gazebo with world {world_path}")
-    gazebo_launch_path = PathJoinSubstitution([
-        get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
+    print(f"Launching Gazebo with world {world_sdf_path}")
+    gz_launch_path = PathJoinSubstitution([
+        get_package_share_directory(PACKAGE_NAME), 'launch', 'gz.launch.py' # using local launch file with fixed debugger call
     ])
     # gz params explained: ruby /opt/ros/jazzy/opt/gz_tools_vendor/bin/gz sim --help
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([gazebo_launch_path]),
+    debugger = LaunchConfiguration('debugger', default="false").perform(context)
+    gdb_server = LaunchConfiguration("gdb_server", default="false").perform(context)
+    gdb_server_port = LaunchConfiguration("gdb_server_port", default="3000").perform(context)
+    
+    actions.append(IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([gz_launch_path]),
         launch_arguments=[
+            ('debugger', debugger),
+            ('gdb_server', gdb_server),
+            ('gdb_server_port', gdb_server_port),
             ('gz_args', [' -r', # run the simulation
                          ' -s', # server only (headless mode)
                          ' -v 4', # verbosity level (0-4)
+                         ' --headless-rendering',
                          ' --render-engine ogre2',
                          ' --render-engine-api-backend vulkan'
                          ' --physics-engine gz-physics-dartsim-plugin ',
-                         world_path])
+                         world_sdf_path])
         ]
-    )
+    ))
     
     # Spawn the robot in Gazebo
     print(f"Spawning the robot...")
-    spawn_robot = Node(package='ros_gz_sim', executable='create',
+    actions.append(Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
                                    '-name', robot_model,
                                    '-z', '0.5'],
-                        output='screen')
+                        output='screen'))
     
     # gz bridge 
     print(f"Starting ROS-GZ bridge...")
     gz_bridge_params_file = os.path.join(get_package_share_directory(PACKAGE_NAME),'config', 'gz_bridge.yaml')
-    ros_gz_bridge = Node(
+    actions.append(Node(
         package="ros_gz_bridge",
         executable="parameter_bridge",
         parameters=[{'config_file': gz_bridge_params_file}],
@@ -135,19 +150,37 @@ def generate_launch_description():
             '-p',
             f'config_file:={gz_bridge_params_file}',
         ]
-    )    
+    ))
+    
+    # can_bridges = []
+    # for cam_suffix in [ 'top', 'front', 'back' ]:
+    #     print(f"Starting ROS-GZ bridge...")
+    #     gz_bridge_params_file = os.path.join(get_package_share_directory(PACKAGE_NAME),'config', f'gz_bridge_cam_{cam_suffix}.yaml')
+    #     cam_gz_bridge = Node(
+    #         package=f"ros_gz_bridge",
+    #         executable="parameter_bridge",
+    #         parameters=[{'config_file': gz_bridge_params_file}],
+    #         name=f'ros_gz_bridge_cam_{cam_suffix}',
+    #         arguments=[
+    #             '--ros-args',
+    #             '-p',
+    #             f'config_file:={gz_bridge_params_file}',
+    #         ]
+    #     )
+    #     can_bridges.append(cam_gz_bridge)
     
     mecanum_remappings = '--ros-args --remap /mecanum_controller/odometry:=/odom --remap /mecanum_controller/reference:=/cmd_vel --remap /mecanum_controller/tf_odometry:=/tf'
     
     # Spawn joint_state_broadcaster
     print(f"Spawning joint state broadcaster...")
-    spawn_jsb = Node(
+    actions.append(Node(
         package='controller_manager',
         executable='spawner',
         arguments=['joint_state_broadcaster',
                    '--controller-ros-args', mecanum_remappings],
         output='screen',
-    )
+    ))
+    
     # , 
 
     # Spawn wheel controllers
@@ -162,7 +195,7 @@ def generate_launch_description():
     # )
     
     print(f"Lading mecanum controller...")    
-    load_mecanum_controller = Node(
+    actions.append(Node(
         package='controller_manager',
         executable='spawner',  # the spawner script/executable
         output='screen',
@@ -171,7 +204,7 @@ def generate_launch_description():
             '--controller-manager', '/controller_manager', # controller manager node name (if not default)
             '--controller-ros-args', mecanum_remappings
         ],
-    )
+    ))
     
     # print(f"Lading top camera controller...")    
     # load_camera_joint_top_controller = Node(
@@ -207,31 +240,31 @@ def generate_launch_description():
     #     arguments=['-d', os.path.join(pkg_path, 'rviz', 'test.rviz')]
     # )
     
-    print(f"Making sim_extras node...")    
-    sim_extras_node = Node(
-        package='simbot_gz',
-        executable='sim_extras_publisher_async.py',
-        name='sim_extras_publisher',
-        parameters=[PathJoinSubstitution([
-            FindPackageShare('simbot_gz'),
-            'config',
-            'sim_extras_config.yaml'
-        ])],
-        output='screen'
-    )
+    # print(f"Making sim_extras node...")    
+    # ld.add_action(Node(
+    #     package='simbot_gz',
+    #     executable='sim_extras_publisher_async.py',
+    #     name='sim_extras_publisher',
+    #     parameters=[PathJoinSubstitution([
+    #         FindPackageShare('simbot_gz'),
+    #         'config',
+    #         'sim_extras_config.yaml'
+    #     ])],
+    #     output='screen'
+    # ))
     
-    print(f"Making range converter node...")    
-    range_converter_node = Node(
-        package=PACKAGE_NAME,
-        executable='laser_to_range_async.py',  
-        name='laser_to_range_converter',
-        parameters=[PathJoinSubstitution([
-            FindPackageShare(PACKAGE_NAME),
-            'config',
-            'range_config.yaml'
-        ])],
-        output='screen'
-    )
+    # print(f"Making range converter node...")    
+    # ld.add_action(Node(
+    #     package=PACKAGE_NAME,
+    #     executable='laser_to_range_async.py',  
+    #     name='laser_to_range_converter',
+    #     parameters=[PathJoinSubstitution([
+    #         FindPackageShare(PACKAGE_NAME),
+    #         'config',
+    #         'range_config.yaml'
+    #     ])],
+    #     output='screen'
+    # ))
 
     # Launch the camera joint service node
     # print(f"Launching camera joint service...")    
@@ -251,26 +284,25 @@ def generate_launch_description():
     # )
 
     # Create launch description and add actions
-    ld = LaunchDescription(ARGUMENTS)
-    ld.add_action(ign_resource_path_meshes)
-    ld.add_action(ign_resource_path_worlds)
-    ld.add_action(ign_resource_path_models)
-    ld.add_action(append_ign_path)
-    ld.add_action(append_gz_path)
-    ld.add_action(node_robot_state_publisher)
-    ld.add_action(gazebo)
-    ld.add_action(spawn_robot)
-    ld.add_action(ros_gz_bridge)
+    
+    
+    # for can_gz_bridge in can_bridges:
+    #     ld.add_action(can_gz_bridge)
     # ld.add_action(spawn_wheel_controllers)
-    ld.add_action(spawn_jsb)
-    ld.add_action(load_mecanum_controller)
+    
+    
     # ld.add_action(load_camera_joint_top_controller)
-    ld.add_action(sim_extras_node)
-    ld.add_action(range_converter_node)
+    
+    
     # ld.add_action(camera_joint_service_node)
     # ld.add_action(set_camera_joint_position_pub_node)
     # ld.add_action(mecanum_tf_republisher_node)
     # ld.add_action(kinematics)
     # ld.add_action(rviz_node)
-    return ld
+    return actions
 
+def generate_launch_description():
+    
+    return LaunchDescription(ARGUMENTS + [
+        OpaqueFunction(function=launch_setup)
+    ])
